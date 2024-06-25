d7cbc3ea31d1626e597875e0a662f135a7d22fbc
Wesley Luyten <me@wesleyluyten.com>
2024-06-25 07:22:25 -0500

    site: set Posthog profiles to always + pageleave

--- site/app/_components/AnalyticsProvider.tsx
+++ site/app/_components/AnalyticsProvider.tsx
@@ -6,8 +6,9 @@ import { PostHogProvider } from 'posthog-js/react';
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only',
+    person_profiles: 'always', // 'always' to create profiles for anonymous users
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    capture_pageleave: true, // Enable pageleave capture
   });
 }
 
