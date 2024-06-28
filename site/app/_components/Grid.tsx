8cf1769b380051576eaadabfe9b26b42441b4904
Wesley Luyten <me@wesleyluyten.com>
2024-06-28 11:31:58 -0500

    site: add no search results banner

--- site/app/_components/Grid.tsx
+++ site/app/_components/Grid.tsx
@@ -9,7 +9,12 @@ type GridProps = {
 export default function Grid({ className, children }: GridProps) {
   return (
     <>
-      <div className={clsx('relative flex-1 border-y -my-1px grid grid-cols-xs sm:grid-cols-sm lg:grid-cols-lg xl:grid-cols-xl bg-putty-light border-ctx-gray text-black set-bg-ctx-putty-light set-border-ctx-gray', className)}>
+      <div
+        className={clsx(
+          'relative border-y -my-1px grid grid-cols-xs sm:grid-cols-sm lg:grid-cols-lg xl:grid-cols-xl bg-putty-light border-ctx-gray text-black set-bg-ctx-putty-light set-border-ctx-gray',
+          className
+        )}
+      >
         <div className="col-start-2 col-end-3 border-x border-ctx-gray">
           <div className="gap-2 lg:gap-3 items-center py-2 px-1 md:p-2 lg:py-3 lg:px-3">
             <div className="block border-ctx-gray bg-putty-light [&amp;>*]:set-border-ctx-gray [&amp;>*]:set-bg-ctx-putty-light text-black">
--- site/app/about/page.tsx
+++ site/app/about/page.tsx
@@ -3,7 +3,7 @@ import Grid from '../_components/Grid';
 export default function About() {
   return (
     <>
-      <Grid>
+      <Grid className="flex-1">
         <h1 className="text-3xl font-bold mb-0.5">About</h1>
         <p className="text-md mb-0.5 md:mr-8">
           Player.style is a platform for creating and sharing media player themes.
