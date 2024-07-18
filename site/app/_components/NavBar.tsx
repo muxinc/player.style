114995a2e8c31abe1601f5ad1d8fe622a18421b9
Wesley Luyten <me@wesleyluyten.com>
2024-07-18 10:11:45 -0500

    site: add mobile nav

--- site/app/_components/NavBar.tsx
+++ site/app/_components/NavBar.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import PlayerStyleLogo from './PlayerStyleLogo';
 import MuxLogo from './MuxLogo';
+import { NavLink } from './NavLink';
+import MobileNav from './MobileNav';
 
 export default function NavBar() {
   return (
     <>
-      <div className="overflow-hidden border-y -my-1px grid grid-cols-xs sm:grid-cols-sm lg:grid-cols-lg xl:grid-cols-xl bg-putty-light border-ctx-gray text-black set-bg-ctx-putty-light set-border-ctx-gray sticky -top-1px z-50">
+      <div className="border-y -my-1px grid grid-cols-xs sm:grid-cols-sm lg:grid-cols-lg xl:grid-cols-xl bg-putty-light border-ctx-gray text-black set-bg-ctx-putty-light set-border-ctx-gray sticky -top-1px z-50">
         <div className="col-start-2 col-end-3 border-x border-ctx-gray">
           <div className="-m-0.5px grid-cols-1 relative h-2 md:h-3 flex items-center justify-between">
             <div className="flex items-center text-sm relative -top-2px">
@@ -21,9 +23,13 @@ export default function NavBar() {
               >
                 <span>BY</span>
                 <span className="sr-only">Made by Mux</span>
-                <MuxLogo className="fill-current cursor-pointer" style={{ width: 36, height: 11 }} />
+                <MuxLogo
+                  className="fill-current cursor-pointer"
+                  style={{ width: 36, height: 11 }}
+                />
               </a>
             </div>
+            <MobileNav />
             <nav
               aria-label="Main"
               data-orientation="horizontal"
@@ -33,20 +39,20 @@ export default function NavBar() {
               <div className="relative">
                 <ul data-orientation="horizontal" className="h-full flex" dir="ltr">
                   <li className="flex items-stretch whitespace-nowrap -mx-0.25 first:ml-0 last:mr-0 xl:mx-0">
-                    <Link
+                    <NavLink
                       className="flex items-center p-1 hover:bg-black hover:text-white"
                       href="/"
                     >
                       Themes
-                    </Link>
+                    </NavLink>
                   </li>
                   <li className="flex items-stretch whitespace-nowrap -mx-0.25 first:ml-0 last:mr-0 xl:mx-0">
-                    <Link
+                    <NavLink
                       className="flex items-center p-1 hover:bg-black hover:text-white"
                       href="/about"
                     >
                       About
-                    </Link>
+                    </NavLink>
                   </li>
                   <li className="flex items-stretch whitespace-nowrap -mx-0.25 first:ml-0 last:mr-0 xl:mx-0">
                     <a
