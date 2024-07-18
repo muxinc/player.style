ff5e95532f191e9f765bef892914f37d8deaaa97
Wesley Luyten <me@wesleyluyten.com>
2024-07-18 12:38:00 -0500

    site: fix by Mux logo

--- site/app/_components/NavBar.tsx
+++ site/app/_components/NavBar.tsx
@@ -10,19 +10,18 @@ export default function NavBar() {
       <div className="border-y -my-1px grid grid-cols-xs sm:grid-cols-sm lg:grid-cols-lg xl:grid-cols-xl bg-putty-light border-ctx-gray text-black set-bg-ctx-putty-light set-border-ctx-gray sticky -top-1px z-50">
         <div className="col-start-2 col-end-3 border-x border-ctx-gray">
           <div className="-m-0.5px grid-cols-1 relative h-2 md:h-3 flex items-center justify-between">
-            <div className="flex items-center text-sm relative -top-2px">
+            <div className="flex items-center text-sm relative -top-1px md:-top-2px">
               <Link className="block relative pl-0.5 sm:pl-1 pr-0.25 md:pr-0.5" href="/">
                 <PlayerStyleLogo className="h-[26px] md:h-[34px]" />
                 <span className="sr-only">Player.style</span>
               </Link>
               <a
-                className="flex gap-3px items-center rounded-1 relative text-putty-light bg-blue-core hover:bg-orange h-[18px] -bottom-[1px] px-[8px] text-[9px] scale-90 md:scale-100"
+                className="relative top-[1px] text-blue-core hover:text-orange"
                 href="https://www.mux.com/"
                 target="_blank"
               >
-                <span>BY</span>
                 <span className="sr-only">Made by Mux</span>
-                <MuxLogo className="fill-current w-[36px] h-[11px]" />
+                <MuxLogo className="fill-current w-[60px] md:w-[70px]" />
               </a>
             </div>
             <MobileNav />
