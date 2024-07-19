dbe95f354d26bef1d9ce43ae766bb104c9c3dd55
Wesley Luyten <me@wesleyluyten.com>
2024-07-19 10:57:24 -0500

    fix: improve color CSS vars & site color picker

--- themes/tailwind-audio/tailwind.config.js
+++ themes/tailwind-audio/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // todo: needs more sub variants because slate-500, slate-700 and slate-900 is used.
+        //       ideally 3 shades of each color would be created via CSS.
+        // primary: 'var(--media-primary-color, rgb(51 65 85 / var(--tw-bg-opacity)))',
+        secondary: 'var(--media-secondary-color, #fff)',
+        accent: 'var(--media-accent-color, rgb(79 70 229))',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/container-queries'),
-  ],
-}
+  plugins: [require('@tailwindcss/container-queries')],
+};
