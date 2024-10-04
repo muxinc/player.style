f5655870c9329532d632b614f170205b1eaa8288
Wesley Luyten <me@wesleyluyten.com>
2024-10-04 12:14:54 -0500

    site: add new chapters VTT file

--- site/media-assets.ts
+++ site/media-assets.ts
@@ -4,16 +4,18 @@ const mediaAssets = {
     src: 'https://stream.mux.com/fXNzVtmtWuyz00xnSrJg4OJH6PyNo6D02UzmgeKGkP5YQ.m3u8',
     poster: 'https://image.mux.com/fXNzVtmtWuyz00xnSrJg4OJH6PyNo6D02UzmgeKGkP5YQ/thumbnail.webp?time=52',
     thumbnails: 'https://image.mux.com/fXNzVtmtWuyz00xnSrJg4OJH6PyNo6D02UzmgeKGkP5YQ/storyboard.vtt',
+    chapters: '/landscape-test-chapters.vtt',
     aspectRatio: 16 / 9,
     title: 'Landscape Promo',
     byline: 'by Mux',
-    duration: 63
+    duration: 63.11
   },
   portrait: {
     playbackId: '1EFcsL5JET00t00mBv01t00xt00T4QeNQtsXx2cKY6DLd7RM',
     src: 'https://stream.mux.com/1EFcsL5JET00t00mBv01t00xt00T4QeNQtsXx2cKY6DLd7RM.m3u8',
     poster: 'https://image.mux.com/1EFcsL5JET00t00mBv01t00xt00T4QeNQtsXx2cKY6DLd7RM/thumbnail.webp',
     thumbnails: 'https://image.mux.com/1EFcsL5JET00t00mBv01t00xt00T4QeNQtsXx2cKY6DLd7RM/storyboard.vtt',
+    chapters: null,
     aspectRatio: 9 / 16,
     title: 'Portrait Promo',
     byline: 'by Mux',
