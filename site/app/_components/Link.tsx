5e29c74643a606f4d84f61af4ed69cc0c6b27705
Darius Cepulis <de.cepulis@gmail.com>
2024-10-07 17:31:32 -0700

    Fix useSearchParams build error

--- site/app/_components/Link.tsx
+++ site/app/_components/Link.tsx
@@ -3,10 +3,10 @@
 import NextLink from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import { ParsedUrlQueryInput } from 'querystring';
-import { ComponentPropsWithoutRef } from 'react';
+import { Suspense, ComponentPropsWithoutRef } from 'react';
 
 type LinkProps = ComponentPropsWithoutRef<typeof NextLink>;
-export default function Link({ href, ...rest }: LinkProps) {
+function ClientLink({ href, ...rest }: LinkProps) {
   const searchParams = useSearchParams();
 
   let _href: LinkProps['href'] =
@@ -34,4 +34,15 @@ export default function Link({ href, ...rest }: LinkProps) {
   }
 
   return <NextLink {...rest} href={_href} />;
+}
+
+// useSearchParams requires Suspense.
+// In SSR it renders fallback.
+// On hydration, ClientLink comes in.
+export default function Link(props: LinkProps) {
+  return (
+    <Suspense fallback={<NextLink {...props} />}>
+      <ClientLink {...props} />
+    <Suspense />
+  )
 }
